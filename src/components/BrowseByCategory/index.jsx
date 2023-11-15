20a3262bc1afb80cd3d0d69b18acd38508d197ea
Martin老師 <tpemartin@gmail.com>
2023-11-15 15:35:45 +0800

    update

---
+++ src/components/BrowseByCategory/index.jsx
@@ -0,0 +1,3 @@
+export default function BrowseByCategory() {
+    return <>  </>
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
 
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'docs')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/apple-events/',
+  build: {
+    outDir,
+    emptyOutDir: true
+  }
 })
