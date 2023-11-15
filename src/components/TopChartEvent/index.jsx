da49f999fb29076bf5e20e0cf45bbaa451272043
papayay <92151907+papayaya123@users.noreply.github.com>
2023-11-15 18:03:31 +0800

    New

--- src/components/TopChartEvent/index.jsx
+++ src/components/TopChartEvent/index.jsx
@@ -4,6 +4,7 @@ import { Loop_Card } from './index_card';
 
 function Card({ text1 ,text2 , num }) {
     return (
+        <Box  className="hide-scroll"  sx={{ width: '100%', height: '100%' ,margin:'5px', overflow:'auto'}}>    
         <Stack direction="row" spacing={2}>
             <Loop_Card num = "1"  text1 = "bbb" text2 = "ccc" 
                         img = "https://is1-ssl.mzstatic.com/image/thumb/44dJzkosAhD0-ugRepvsfw/738x416.webp"/>
@@ -22,6 +23,7 @@ function Card({ text1 ,text2 , num }) {
             <Loop_Card num = "1"  text1 = "bbb" text2 = "ccc" 
                         img = "https://is1-ssl.mzstatic.com/image/thumb/44dJzkosAhD0-ugRepvsfw/738x416.webp"/>
         </Stack>
+        </Box>
     );
 }
 
@@ -37,7 +39,7 @@ export default function TopChartEvent() {
 // 替换为实际的图片路径
 
     return (
-        <Box sx = {{backgroundColor : "gray" , color : "white" , height:"350px" , width : '5000px'}}>
+        <Box sx = {{backgroundColor : "gray" , color : "white" , height:"350px"}}>
             <Stack direction="column" spacing={2}>
                 <Typography variant="h4">
                     Top Chart TV Shows <ArrowForwardIosIcon />
